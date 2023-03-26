3f8f3e2b2a14208ece6af327bdd1f16618e5731a
srijerry <srikumar.jcarrers.com>
2023-03-27 01:20:34 +0530

    added GH actions anf tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -7,21 +7,21 @@ resource "aws_vpc" "vpc" {
 resource "aws_subnet" "sn1" {
   cidr_block              = "10.0.1.0/24"
   vpc_id                  = aws_vpc.vpc.id
-  availability_zone       = "us-east-2a"
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "sn2" {
   cidr_block              = "10.0.2.0/24"
   vpc_id                  = aws_vpc.vpc.id
-  availability_zone       = "us-east-2b"
+  availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "sn3" {
   cidr_block              = "10.0.3.0/24"
   vpc_id                  = aws_vpc.vpc.id
-  availability_zone       = "us-east-2c"
+  availability_zone       = "us-east-1c"
   map_public_ip_on_launch = true
 }
 
